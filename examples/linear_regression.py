9dcd1451fd9e8bf7b1ca62fd470a2541e8ce7490
rushter <me@rushter.com>
2015-09-08 06:28:11 +0300

    Add linear regression

--- examples/linear_regression.py
+++ examples/linear_regression.py
@@ -1,14 +1,31 @@
 # Author: rushter <me@rushter.com>
-
+from sklearn.cross_validation import train_test_split
 from mla.datasets import *
-from mla.regression.linear import LinearRegression
+from mla.metrics.scoring import *
+from mla.linear.regression import LinearRegression
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 
 def boston_example():
     X, y = load_boston()
-    model = LinearRegression()
-    model.fit(X, y)
-    model.predict()
+    # X, y = load_lin()
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=1111)
+
+    # # L = LinearRegressionSK()
+    # L = SGDRegressor(n_iter=10000, learning_rate=0.0001)
+    #
+    # L.fit(X_train, y_train)
+    # predictions = L.predict(X_test)
+    # print(predictions)
+    # print(root_mean_squared_log_error(y_test, predictions))
+
+    model = LinearRegression(alpha=0.000001, max_iters=10000, gradient_type=1)
+    model.fit(X_train, y_train)
+    predictions = model.predict(X_test)
+    print(root_mean_squared_log_error(y_test, predictions))
 
 
 if __name__ == '__main__':
---
+++ mla/linear/regression.py
@@ -0,0 +1,114 @@
+# Author: rushter <me@rushter.com>
+
+from mla.base import Base
+from mla.metrics.scoring import squared_error
+import numpy as np
+import logging
+
+
+class LinearRegression(Base):
+    def __init__(self, max_iters=1000, alpha=0.0001, tolerance=0.0001, gradient_type=0):
+        """
+
+        :param alpha: learning rate
+        """
+
+        self.gradient_type = gradient_type
+        self.tolerance = tolerance
+        self.alpha = alpha
+        self.max_iters = max_iters
+        self.y_required = True
+        self.errors = []
+        self.theta = []
+
+    def fit(self, X, y=None):
+        super(LinearRegression, self).fit(X, y)
+
+        # Weights
+        self.theta = np.zeros(shape=(self.n_features + 1, 1))
+
+        # Add an intercept columsn
+        self.X = self._add_intercept(self.X)
+
+        self._train()
+
+    def _add_intercept(self, X):
+        b = np.ones([X.shape[0], 1])
+        return np.concatenate([b, X], axis=1)
+
+    def _train(self):
+        self.theta, self.errors = self._gradient_descent(self.X, self.y)
+        logging.info(' Theta: %s' % self.theta.flatten())
+
+    def _predict(self, X=None):
+        X = self._add_intercept(X)
+        return [max(0, x) for x in self._get_predictions(X)]
+
+    def _get_predictions(self, X=None, theta=None):
+
+        if X is None:
+            X = self.X
+
+        if theta is None:
+            theta = self.theta
+
+        return X.dot(theta).sum(axis=1)
+
+    def _compute_cost(self, X, y, theta):
+        prediction = X.dot(theta)
+        error = squared_error(y, prediction).sum()
+        return error
+
+    def _gradient_descent(self, X, y):
+        theta = self.theta
+        errors = [self._compute_cost(X, y, theta)]
+
+        if self.gradient_type == 1:
+            gradient = self._step_batch_gradient
+        elif self.gradient_type == 2:
+            gradient = self._step_w_gradient
+        else:
+            gradient = self._step_gradient
+
+        for i in range(1, self.max_iters + 1):
+
+            theta = gradient(X, y, theta, self.alpha)
+
+            errors.append(self._compute_cost(X, y, theta))
+            logging.info('Iteration %s, error %s' % (i, errors[i]))
+
+            # TODO: abs?
+            error_diff = (errors[i - 1] - errors[i])
+
+            if error_diff < self.tolerance:
+                logging.info('Convergence has reached.')
+                break
+        return theta, errors
+
+    def _step_gradient(self, X, y, theta, alpha):
+        for i in range(self.n_samples):
+            prediction_difference = np.dot(X[i, :], theta)[0] - y[i]
+            gradient = (2 / self.n_samples) * X[i, :] * prediction_difference
+            gradient = np.reshape(gradient, (gradient.size, 1))
+            theta = theta - (alpha * gradient)
+        return theta
+
+    def _step_batch_gradient(self, X, y, theta, alpha):
+        gradient = 0
+        for i in range(self.n_samples):
+            prediction_difference = np.dot(X[i, :], theta) - y[i]
+            gradient += (2 / self.n_samples) * X[i, :] * prediction_difference
+
+        gradient = np.reshape(gradient, (len(gradient), 1))
+        theta = theta - (alpha * gradient)
+        return theta
+
+    def _step_w_gradient(self, X, y, theta, alpha):
+
+        prediction = self._get_predictions()
+        m = X.size
+        for it in range(len(theta)):
+            temp = X[:, it].copy()
+            errors = (prediction - y) * temp
+            theta[it] -= alpha * (2 / m) * errors.sum()
+        return theta
